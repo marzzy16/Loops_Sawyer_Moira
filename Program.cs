dd09ad4446767068da1bd449e88c989040b7cde5
marzzy16 <114106672+marzzy16@users.noreply.github.com>
2023-04-11 14:02:49 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Loops_Sawyer_Moira
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // asks user why they want to make games
+            Console.WriteLine("Why do you want to make games?");
+            string response = Console.ReadLine();
+            Console.WriteLine($"The sentence, \"{response}\" has {CountNumberOfSpaces(response)} spaces in it.");
+            //asks user to enter a whole number
+            Console.WriteLine("Please enter a whole number!");
+            string respnse2 = Console.ReadLine();
+            Console.WriteLine($"The sum of the individual digits of {respnse2} is {SumOfDigits(respnse2)}!");
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sentence">The sentence to count number of spaces in</param>
+        /// <returns>Number of spaces in the sentence</returns>
+        static int CountNumberOfSpaces(string sentence)
+        {
+            int numberOfSpaces = 0;
+            foreach(char character in sentence)
+            {
+                if(character == ' ')
+                    numberOfSpaces++;
+            }
+
+            return numberOfSpaces;
+        }
+
+        static int SumOfDigits(string number)
+        {
+            int sum = 0;
+            foreach(char dight in number)
+            {
+                sum +=(int) Char.GetNumericValue(dight);
+            }
+
+            return sum;
+        }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Loops_Sawyer_Moira
+ 
